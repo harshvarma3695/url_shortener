8aeed53c99b6bdb0bb52c6cccf5e4ea6c893e8eb
harshvarma3695 <hv87805@gmail.com>
2026-07-10 16:04:10 +0530

    Fixed shortId reference error

--- controllers/url.js
+++ controllers/url.js
@@ -1,26 +1,35 @@
 import shortid from "shortid";
-import { url } from "../models/Url.js"
-export const shortUrl=async(req,res)=>{
-    const longUrl=req.body.longUrl;
-    const shortCode=shortid.generate();
+import { url } from "../models/Url.js";
 
-    const shortUrl = `http://localhost:1000/${shortCode}`;
+export const shortUrl = async (req, res) => {
+  const longUrl = req.body.longUrl;
 
-    const newUrl=new url({shortCode,longUrl})
-    await newUrl.save();
+  const shortCode = shortid.generate();
 
-    console.log("short saved =",newUrl);
-    res.render("index.ejs",{shortUrl})
+  const shortUrl = `${process.env.BASE_URL}/${shortCode}`;
 
-}
+  const newUrl = new url({
+    shortCode,
+    longUrl,
+  });
 
-export const getOriginalUrl=async(req,res)=>{
-    const shortCode=req.params.shortCode
-    const originalUrl=await url.findOne({shortCode})
+  await newUrl.save();
 
-    if(originalUrl){
-        res.redirect(originalUrl.longUrl)
-    }else{
-        res.json({message:'invalid shortUrl'})
-    }
-}
+  console.log("Short URL saved =", newUrl);
+
+  res.render("index.ejs", { shortUrl });
+};
+
+export const getOriginalUrl = async (req, res) => {
+  const shortCode = req.params.shortCode;
+
+  const originalUrl = await url.findOne({ shortCode });
+
+  if (originalUrl) {
+    return res.redirect(originalUrl.longUrl);
+  }
+
+  res.status(404).json({
+    message: "Invalid Short URL",
+  });
+};
