efb6cd563109400612770da4d260dca57d7f2a83
harshvarma3695 <hv87805@gmail.com>
2026-07-10 17:11:17 +0530

    Add .gitignore and environment configuration

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { shortUrl,getOriginalUrl } from './controllers/url.js';
+import dotenv from "dotenv";
+dotenv.config();
 const app=express();
 app.use(express.urlencoded({extended:true}))
 
-mongoose.connect("mongodb+srv://harshvarma3695:hv123var@cluster0.9w1d736.mongodb.net/",{
-    dbName:'url_short',
-},).then(
-    console.log("mongoDB are connected....")
-    
-).catch((err)=>{
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
     console.log(err);
-    
-})
+  });
 
 app.get('/',(req,res)=>{
     res.render('index.ejs',{shortUrl:null})
@@ -23,8 +24,8 @@ app.post('/short-url',shortUrl)
 app.get("/:shortCode",getOriginalUrl);
 
 
-const port=1000;
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-    
-})
+const port = process.env.PORT || 1000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
